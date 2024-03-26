53e9b1f4447644590e89d6df0e02a87c67e16fcf
jonmargolin <jonmargolin@gmail.com>
2024-03-26 09:24:46 +0200

    Add params to get result (#11)
    * add  scan parmas to getresulte
    
    * set getResult url with out parms
    
    * add defualt duration value
    
    * add scan params
    
    * fix url query params
    
    * fix init context to not used
    
    ---------
    
    Co-authored-by: admin <admin@admins-MBP.lan>

--- src/components/scanInfo/scanContext.tsx
+++ src/components/scanInfo/scanContext.tsx
@@ -31,7 +31,7 @@ const initialState: IScanContextValue = {
   totalEmailSize: "",
   totalNewsLetterFound: 0,
   totalNewsLetterMail: 0,
-  duration: ReadStatus.Active,
+  duration: ReadStatus.notInUse,
 };
 
 const reducer = (
