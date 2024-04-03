6dcf1a7d4f6839086a3a3e0e8bb637459b20030e
jonmargolin <jonmargolin@gmail.com>
2024-04-03 10:19:13 +0300

    Add progres bar (#23)
    * chnage email deleted condtion
    
    * add progress bar to loading page
    
    ---------
    
    Co-authored-by: admin <admin@admins-MBP.lan>

--- src/components/LoaderPage.tsx
+++ src/components/LoaderPage.tsx
@@ -1,14 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Lottie from "react-lottie";
 import { checkStatus } from "../service/api";
 import { useNavigate } from "react-router-dom";
 import animationData from "./../assets/Loader.json";
 import { useTranslation } from "react-i18next";
 import { loadingRemembers } from "./const";
+import { Progress } from "@chakra-ui/react";
 
 const LoaderPage = () => {
   const naviget = useNavigate();
   const { t } = useTranslation();
+  const [loadingProgress, setLoadingProgress] = useState(0);
   const defaultOptions = {
     loop: true,
     autoplay: true,
@@ -19,6 +21,7 @@ const LoaderPage = () => {
   };
   const renderTextRemembers = (): React.ReactNode => {
     const list: React.ReactNode[] = [];
+
     loadingRemembers.forEach((item, index) => {
       list.push(
         <p className="mb-2" key={`remb${index}`}>
@@ -35,6 +38,8 @@ const LoaderPage = () => {
       try {
         const { idsLength, initLength } = await checkStatus();
         if (initLength !== idsLength) {
+          const updateNumber = (idsLength / initLength) * 100;
+          setLoadingProgress(updateNumber);
           setTimeout(() => fetchData(), 5000);
         } else {
           naviget("/scan");
@@ -50,7 +55,10 @@ const LoaderPage = () => {
       <p className="mt-10 text-24 text-black">{t("loadingPage.title")} </p>
       <p className="text-16 text-black">{t("loadingPage.subTitle")} </p>
       <div className="mt-10">
-        <Lottie options={defaultOptions} height={200} width={200} />;
+        <Lottie options={defaultOptions} height={200} width={200} />
+      </div>
+      <div className="flex w-full ps-5 pe-5">
+        <Progress className="w-full" size="sm" value={loadingProgress} />
       </div>
       <p className="mt-10 mb-2 text-base ps-5 pe-5">
         {t("loadingPage.remembers")}
